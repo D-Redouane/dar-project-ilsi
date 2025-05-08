032cb609193090e178da939a82d2cd8930c358e6
D-Redouane <D-Redouane@users.noreply.github.com>
2025-05-08 12:21:02 +0100

    fix cluster side for pc2

--- sensor/ClusterServer.java
+++ sensor/ClusterServer.java
@@ -7,10 +7,12 @@ import java.rmi.registry.LocateRegistry;
 public class ClusterServer {
     public static void main(String[] args) {
         try {
+            // Set the hostname for RMI to PC2's IP
+            System.setProperty("java.rmi.server.hostname", "172.16.144.108");
             LocateRegistry.createRegistry(2095);
             Cluster cluster = new Cluster();
-            Naming.rebind("rmi://localhost:2095/Cluster", cluster);
-            System.out.println("Cluster is running and bound to RMI registry on port 2095.");
+            Naming.rebind("rmi://172.16.144.108:2095/Cluster", cluster);
+            System.out.println("Cluster is running and bound to RMI registry on 172.16.144.108:2095.");
         } catch (RemoteException | java.net.MalformedURLException e) {
             e.printStackTrace();
         }
--- sensor/StartClusterGUI.java
+++ sensor/StartClusterGUI.java
@@ -1,26 +1,22 @@
 package sensor;
 
-
 import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
 
 public class StartClusterGUI {
     public static void main(String[] args) {
         try {
-            // Create and show the cluster GUI
+            // Set the hostname for RMI
+            System.setProperty("java.rmi.server.hostname", "172.16.144.108");
             LocateRegistry.createRegistry(2095);
             Cluster cluster = new Cluster();
-            Naming.rebind("rmi://localhost:2095/Cluster", cluster);
-            System.out.println("Cluster is running and bound to RMI registry on port 2095.");
-
-
-            //Lookup the cluster on localhost (or server IP)
-            //ClusterInterface cluster = (ClusterInterface) Naming.lookup("rmi://localhost:2095/Cluster");
+            Naming.rebind("rmi://172.16.144.108:2095/Cluster", cluster);
+            System.out.println("Cluster is running and bound to RMI registry on 172.16.144.108:2095.");
 
             ClusterGUI clusterGUI = new ClusterGUI(cluster);
             clusterGUI.setVisible(true);
-            
-            System.out.println("Cluster GUI is running.");
+
+            System.out.println("Cluster GUI is running on 172.16.144.108.");
         } catch (Exception e) {
             e.printStackTrace();
         }
